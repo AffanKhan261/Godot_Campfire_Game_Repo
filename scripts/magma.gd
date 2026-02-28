1544067e5ba52e934a846da3393027db290f653d
24kexisting <marcuslee1506@gmail.com>
2026-03-01 08:25:15 +1000

    prologue full level structuring, added light orb, lava and magma functionality

---
+++ scripts/magma.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+var magma_damage_check = true
+
+func _on_body_entered(body: Node2D) -> void:
+	magma_damage_check = true
+	while GlobalVar.HEALTH > 0 and magma_damage_check == true:
+		GlobalVar.HEALTH -= 100
+		print(GlobalVar.HEALTH)
+		await get_tree().create_timer(0.25).timeout
+	print("out of loop check")
+
+
+
+func _on_body_exited(body: Node2D) -> void:
+	magma_damage_check = false
