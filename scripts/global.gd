8101c521c8f85f24f65a5d074af60015b9c773ce
24kexisting <marcuslee1506@gmail.com>
2026-02-28 18:39:17 +1000

    added a 'health global variable', added death anim check at bottom of miner_player script, find a way to stop movement and restart game properly (send to main menu? checkpoint?)

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,3 +1,4 @@
 extends Node
 
 var walljump_access = true
+var HEALTH: float = -1000.0
--- scripts/miner_player.gd
+++ scripts/miner_player.gd
@@ -243,30 +243,36 @@ func _update_facing_and_flip(delta: float) -> void:
 
 # ---------------- ANIMATIONS ----------------
 func _update_animations() -> void:
-	# Don't override the attack animation while attacking
-	if is_attacking:
-		return
+	if GlobalVar.HEALTH > 0:
+		# Don't override the attack animation while attacking
+		if is_attacking:
+			return
 
-	# Use "walk" for dash
-	if is_dashing:
-		if animated_sprite.animation != "walk":
-			animated_sprite.play("walk")
-		return
-
-	if is_on_floor():
-		if absf(velocity.x) < 5.0:
-			if animated_sprite.animation != "idle":
-				animated_sprite.play("idle")
-		else:
+		# Use "walk" for dash
+		if is_dashing:
 			if animated_sprite.animation != "walk":
 				animated_sprite.play("walk")
+			return
+
+		if is_on_floor():
+			if absf(velocity.x) < 5.0:
+				if animated_sprite.animation != "idle":
+					animated_sprite.play("idle")
+			else:
+				if animated_sprite.animation != "walk":
+					animated_sprite.play("walk")
+		else:
+			if velocity.y < 0.0 and animated_sprite.sprite_frames.has_animation("jump"):
+				if animated_sprite.animation != "jump":
+					animated_sprite.play("jump")
+			elif velocity.y >= 0.0 and animated_sprite.sprite_frames.has_animation("fall"):
+				if animated_sprite.animation != "fall":
+					animated_sprite.play("fall")
 	else:
-		if velocity.y < 0.0 and animated_sprite.sprite_frames.has_animation("jump"):
-			if animated_sprite.animation != "jump":
-				animated_sprite.play("jump")
-		elif velocity.y >= 0.0 and animated_sprite.sprite_frames.has_animation("fall"):
-			if animated_sprite.animation != "fall":
-				animated_sprite.play("fall")
+		#stops all player input
+		animated_sprite.play("death")
+		await get_tree().create_timer(2).timeout
+		#get_tree().reload_current_scene()
 
 func get_facing() -> int:
 	return facing
