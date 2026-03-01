a605974c2c06b7f108602b3bca7c258dd2df3224
24kexisting <marcuslee1506@gmail.com>
2026-03-01 17:30:26 +1000

    ya

--- scripts/level_4_gate.gd
+++ scripts/level_4_gate.gd
@@ -1,11 +1,15 @@
 extends Area2D
 
+@onready var animplayer = $"../AnimationPlayer"
+
 var whitegate_anim_enabler = false
 func _on_body_entered(body: Node2D) -> void:
 	if whitegate_anim_enabler == false:
 		$AnimatedSprite2D.play()
 		whitegate_anim_enabler = true
 		await get_tree().create_timer(3).timeout
+		animplayer.play("fall")
+		await get_tree().create_timer(0.4).timeout
 		get_tree().change_scene_to_file("res://scenes/control.tscn")
 	else:
 		pass
