ff3cb8906f53e499903ec386a6aff4325294a390
24kexisting <marcuslee1506@gmail.com>
2026-03-01 11:46:25 +1000

    switched level 2 and 3 files and their music, fixed the gate bug on level 1

--- scripts/level_1_gate.gd
+++ scripts/level_1_gate.gd
@@ -6,5 +6,7 @@ func _on_body_entered(body: Node2D) -> void:
 		pass
 		$AnimatedSprite2D.play()
 		lightgate_anim_enabler = true
+		await get_tree().create_timer(3).timeout
+		get_tree().change_scene_to_file("res://scenes/level_2.tscn")
 	else:
 		pass
